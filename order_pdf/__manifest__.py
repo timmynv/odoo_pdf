7cc31091c5ebb601fb14a018b7edb7d7a9ae5282
tinh.nguyen <tinhnguyentt96@gmail.com>
2024-01-17 10:27:37 +0700

    add

---
+++ order_pdf/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'TM: Orders Print',
+    'version': '17.1',
+    'author': 'Timmy Nguyen (tinhnguyentt96@gmail.com)',
+    'summary': 'Module help custom SO PDF',
+    'website': 'https://pedal.vn/',
+    'category': 'Base',
+    'description': """
+    """,
+    'depends': ['sale_management'],
+    'data': [
+        'views/order_pdf_views.xml'
+    ],
+    'images': [],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'license': 'OPL-1',
+}
